2a807395cb6de1f26ee5109903b88fc18306a850
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-01-18 00:22:28 +0530

    add create action to add new todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,12 +1,24 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant_string }.join("\n")
+    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
 
   def show
     todo_id = params[:id]
     todo = Todo.find(todo_id)
     render plain: todo.to_pleasant_string
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+
+    render plain: "Your todo was created with the id #{new_todo.id}"
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ActiveRecord::Base
   def to_pleasant_string
-    "#{id}. #{completed ? '[X]': '[ ]'} #{due_date.to_s(:long)} \t | #{todo_text} "
+    "#{id}. #{completed ? "[X]" : "[ ]"} #{due_date.to_s(:long)} \t | #{todo_text} "
   end
 end
