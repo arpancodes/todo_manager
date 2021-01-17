97ea236cf9bb88c1452143a30204388b2cd5c9f8
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-01-18 01:39:05 +0530

    create show action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    users_list = User.all.map { |user| user.display_user }.join("")
+    users_list = User.all.map { |user| user.display_users_list }.join("")
     to_render = "<h2>List of users on our platform!</h2><ol>#{users_list}</ol>"
     render html: to_render.html_safe
   end
@@ -17,5 +17,11 @@ class UsersController < ApplicationController
       password: password,
     )
     render html: "<h1>Hey #{user.name}, Welcome to Todo management!</h1>".html_safe
+  end
+
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render html: user.display_user.html_safe
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  def display_user
+  def display_users_list
     "<li>#{name} - #{email}</li>"
+  end
+
+  def display_user
+    "<h2>Details about #{name}:</h2><ul><li>Name: #{name}</li><li>Email: #{email}</li></ul>"
   end
 end
