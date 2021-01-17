c663678143e2677ce863195b39c135b38226266e
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-01-18 00:57:25 +0530

    add update action to update the completed status

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -23,4 +23,13 @@ class TodosController < ApplicationController
 
     render plain: "Your todo was created with the id #{new_todo.id}"
   end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "Updated todo completed status to #{completed}"
+  end
 end
