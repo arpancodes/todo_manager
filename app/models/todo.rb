f626b7e69bbafe49361ec6b84504eb2225adb70e
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-01-20 03:57:39 +0530

    display the list of todos as per specification

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,32 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    "#{id}. #{completed ? "[X]" : "[ ]"} #{due_date.to_s(:long)} \t | #{todo_text} "
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue_todos
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today_todos
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later_todos
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed_todos
+    all.where(completed: true)
+  end
+
+  def self.add_task(task)
+    create!(todo_text: task[:todo_text], due_date: Date.today + task[:due_in_days], completed: false)
+  end
+
+  def self.mark_as_complete!(todo_id)
+    todo = find(todo_id)
+    todo.completed = true
+    todo.save
+    todo
   end
 end
